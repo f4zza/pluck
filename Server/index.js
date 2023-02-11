5ea2c029141ed9738c50262ab49656892b34ef7b
f4zza <lazie.piscean@live.co.uk>
2023-02-11 23:18:35 +0300

    finished mongodb

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors'
-import connectDB from './mongodb/connect'
+import connectDB from './mongodb/connect.js'
 
 dotenv.config()
 
@@ -15,7 +15,12 @@ app.get('/', async(req,res)=> {
 })
 
 const startServer = async()=> {
-    app.listen(8080, ()=> console.log(`Server is running on port http://localhost:8080`))
+    try{
+        connectDB(process.env.MONGODB_URL)
+        app.listen(8080, ()=> console.log(`Server is running on port http://localhost:8080`))
+    }catch(error){
+        console.log(error)
+    } 
 }
 
 startServer()
