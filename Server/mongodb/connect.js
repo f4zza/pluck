bbc757c0ae58f0025ba41595a732ade45aed7d0b
f4zza <lazie.piscean@live.co.uk>
2023-02-13 14:31:32 +0300

    fixes

--- Server/mongodb/connect.js
+++ Server/mongodb/connect.js
@@ -1,10 +1,13 @@
 import mongoose from 'mongoose'
 
 const connectDB = (url) => {
-    mongoose.set('strictQuery', true)
-    mongoose.connect(url)
-    .then(()=> console.log(`Connected to Database`))
-    .catch((err)=> console.log(err))
+  mongoose.set('strictQuery', true)
+  mongoose.connect(url)
+    .then(() => console.log('connected to database'))
+    .catch((err) => {
+      console.error('failed to connect to database')
+      console.error(err)
+    })
 }
 
 export default connectDB
