132bbb148ab720068ecaf5ccab17e6a3712add06
f4zza <lazie.piscean@live.co.uk>
2023-02-13 14:50:20 +0300

    updated link

--- Client/src/page/Home.jsx
+++ Client/src/page/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('https://pluck-fx22.onrender.com/api/v1/post', {
+      const response = await fetch('https://pluck-chod.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
